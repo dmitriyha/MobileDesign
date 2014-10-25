8a4bb463bf7af6770e16639b30917546b60e1303
Matti Raudasoja <matti.raudasoja@gmail.com>
2014-10-25 19:04:48 +0300

    Made some modifications to the answer buttons Question activity is now 50% ready needs buttons generation and answer checking+

--- app/src/main/java/fi/metropolia/healthquiz/activities/Question.java
+++ app/src/main/java/fi/metropolia/healthquiz/activities/Question.java
@@ -2,35 +2,93 @@ package fi.metropolia.healthquiz.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
 import fi.metropolia.healthquiz.R;
+import fi.metropolia.healthquiz.customui.AnswerButton;
+import fi.metropolia.healthquiz.model.AnswerDataSource;
+import fi.metropolia.healthquiz.model.AnswerObject;
+import fi.metropolia.healthquiz.model.QuestionDataSource;
+import fi.metropolia.healthquiz.model.QuestionObject;
 
 public class Question extends Activity {
+
+    private static String TAG = QuestionGroupSelection.class.getCanonicalName();
+    TextView questionTextView;
+    LinearLayout answerButtonContainer;
+    TextView pointsTextView;
+    QuestionDataSource questionDataSource;
+    AnswerDataSource answerDataSource;
+    private long questionGroup;
+    private long points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
-    }
 
+        questionGroup = getIntent().getExtras().getLong("selected_question_group");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.question, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("points")) {
+                points = savedInstanceState.getLong("points");
+            }
+        } else {
+            points = 0;
         }
-        return super.onOptionsItemSelected(item);
+
+        questionTextView = (TextView) findViewById(R.id.question);
+        answerButtonContainer = (LinearLayout) findViewById(R.id.answerButtonContainer);
+        pointsTextView = (TextView) findViewById(R.id.points);
+
+        questionDataSource = new QuestionDataSource(this);
+        answerDataSource = new AnswerDataSource(this);
+
+        try {
+            questionDataSource.open();
+            answerDataSource.open();
+        } catch (SQLException e) {
+            Log.i(TAG, e.getMessage().toString());
+            e.printStackTrace();
+        }
+
+        updatePoints();
+        setupNewQuestion();
+
+
     }
+
+
+    private void setupNewQuestion() {
+
+        List<QuestionObject> questionGroupObjectList = questionDataSource.getQuestionByGroup(questionGroup);
+
+        Random random = new Random();
+
+        QuestionObject question = questionGroupObjectList.get(random.nextInt(questionGroupObjectList.size()));
+
+        questionTextView.setText(question.getQuestion());
+
+        List<AnswerObject> answers = answerDataSource.getAnswersByQuestion(question.getID());
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (AnswerObject answer : answers) {
+
+            answerButtonContainer.addView(new AnswerButton(this, answer, null, R.style.answer_button), lp);
+
+        }
+    }
+
+    private void updatePoints() {
+        pointsTextView.setText(Long.toString(points));
+    }
+
+
 }
---
+++ app/src/main/java/fi/metropolia/healthquiz/customui/AnswerButton.java
@@ -0,0 +1,40 @@
+package fi.metropolia.healthquiz.customui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ToggleButton;
+
+import fi.metropolia.healthquiz.model.AnswerObject;
+
+/**
+ * Created by Matti on 25.10.2014.
+ */
+public class AnswerButton extends ToggleButton {
+
+    private AnswerObject answer;
+
+    public AnswerButton(Context context, AnswerObject answer, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.answer = answer;
+        setupButton();
+    }
+
+    public AnswerButton(Context context, AnswerObject answer, AttributeSet attrs) {
+        super(context, attrs);
+        this.answer = answer;
+        setupButton();
+    }
+
+    public AnswerButton(Context context, AnswerObject answer) {
+        super(context);
+        this.answer = answer;
+
+        setupButton();
+    }
+
+    private void setupButton() {
+        this.setTextOn(answer.getAnswer());
+        this.setTextOff(answer.getAnswer());
+    }
+
+}
